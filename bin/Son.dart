0becaf548af6706809b14295af3bcc7c725fd8d3
petosawny <93099971+petosawny@users.noreply.github.com>
2022-12-15 04:33:17 +0200

    Update Son.dart

--- bin/Son.dart
+++ bin/Son.dart
@@ -1,9 +1,10 @@
 import 'Human.dart';
+// class called Sum extends from the class Calculator
 
 class Son extends Human {
   Son(this.sName, this.sDateOfBirth)
       : super(name: sName, dateOfBirth: sDateOfBirth); //constructor
-  //variables
+  // define variables
 
   String? sName;
 
@@ -12,26 +13,30 @@ class Son extends Human {
   // Abstract Functions from Master Class
 
   @override
+  // fun to print statement
   void drink() {
     print("$name drink milk before bed");
   }
 
   @override
+  // fun to print statement
   void eat() {
     print("$name eat dinner before studying");
   }
 
   @override
+  // fun to print statement
   void move() {
     print("$name move 1Km to reach the club");
   }
 
   @override
+  // fun to print statement
   void sleep() {
     print("$name sleep at least 8h a day");
   }
 
-//function to get name and date of birth
+//function to get name and date of birth and print statement
   void getInfo() {
     print("your name is $name and your birth date is $dateOfBirth");
   }
