cda01a670457b4f4d79abf887ba933736d3f0074
petosawny <93099971+petosawny@users.noreply.github.com>
2022-12-15 04:34:29 +0200

    Update Employee.dart

--- bin/Employee.dart
+++ bin/Employee.dart
@@ -1,30 +1,36 @@
+// import packages and classes
 
 import 'Human.dart';
+// class called Employee extends from the class Human
 
 class Employee extends Human {
   Employee(this.eName) : super(name: eName); //constructor
-  //variables
+  // define variables
 
   String? eName;
 
   // Abstract Functions from Master Class
 
   @override
+  // fun to print statement
   void drink() {
     print("$name drink milk before bed");
   }
 
   @override
-  void eat() {
+  // fun to print statement
+   void eat() {
     print("$name eat dinner at 9");
   }
 
   @override
+  // fun to print statement
   void move() {
     print("$name move 1Km to reach the School");
   }
 
   @override
+  // fun to print statement
   void sleep() {
     print("$name sleep at 11pm every day");
   }
