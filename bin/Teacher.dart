2d386e70ded493c59f061e9343a8e48d7f1d2611
petosawny <93099971+petosawny@users.noreply.github.com>
2022-12-15 04:32:03 +0200

    Update Teacher.dart

--- bin/Teacher.dart
+++ bin/Teacher.dart
@@ -1,29 +1,35 @@
+// import packages and classes
 import 'Human.dart';
+// class called Sum extends from the class Calculator
 
 class Teacher extends Human {
   Teacher(this.tName) : super(name: tName); //constructor
-  //variables
+  // define variables
 
   String? tName;
 
   // Abstract Functions from Master Class
 
   @override
+  // fun to print statement
   void drink() {
     print("$name drink milk before bed");
   }
 
   @override
+  // fun to print statement
   void eat() {
     print("$name eat dinner at 9");
   }
 
   @override
+  // fun to print statement
   void move() {
     print("$name move 1Km to reach the School");
   }
 
   @override
+  // fun to print statement
   void sleep() {
     print("$name sleep at 11pm every day");
   }
